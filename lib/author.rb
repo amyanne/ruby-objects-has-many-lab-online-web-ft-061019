f30b7ff3511ba4da9dbcf87c6080bad453f3f88f
Amy Westcott <amy.westcott@yahoo.com>
2019-06-26 19:30:05 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,13 +1,13 @@
 require 'pry'
 class Author 
   attr_accessor :name, :posts
-  
+  @@post_count = 0
   def initialize(name)
     @name = name
     @posts = []
   end
   
-  def add_post(post)
+  def add_post(post) #ASK BEN
     @posts << post
     post.author = self
     @@post_count +=1
