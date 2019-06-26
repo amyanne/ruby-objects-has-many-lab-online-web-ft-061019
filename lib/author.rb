323055871e0523541a29ec4925bfd83751fff3a4
Amy Westcott <amy.westcott@yahoo.com>
2019-06-26 19:22:49 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -6,9 +6,9 @@ class Author
     @posts = []
   end
   
-  def add_posts(posts)
-    @posts << posts 
-    posts.author = self 
+  def add_post(post)
+    @posts << post
+    post.author = self 
     
   end 
 end 
